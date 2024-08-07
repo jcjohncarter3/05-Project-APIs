2b0cd9cf5ced03308897565d91c333e3d70def6c
jcjohncarter3 <jcjohncarter3@gmail.com>
2024-08-07 18:15:32 -0500

    Defined Lines 52 - 58

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -47,16 +47,16 @@ function createTaskCard(task) {
 
     // TODO: set card background color based on due date
         // Example: 
-        // if (project.dueDate && project.status !== 'done') {
-        // const now = dayjs();
-        // const taskDueDate = dayjs(project.dueDate, 'DD/MM/YYYY');
-        // if (now.isSame(taskDueDate, 'day')) {
-        //   taskCard.addClass('bg-warning text-white');
-        // } else if (now.isAfter(taskDueDate)) {
-        //   taskCard.addClass('bg-danger text-white');
-        //   cardDeleteBtn.addClass('border-light');
-        // }
-        //   }
+         if (task.dueDate && task.status !== 'done') {
+         const now = dayjs();
+         const taskDueDate = dayjs(task.dueDate, 'DD/MM/YYYY');
+         if (now.isSame(taskDueDate, 'day')) {
+           taskCard.addClass('bg-warning text-white');
+         } else if (now.isAfter(taskDueDate)) {
+          taskCard.addClass('bg-danger text-white');
+          cardDeleteBtn.addClass('border-light');
+         }
+           }
 
     // TODO: append card elements
         // Example: 
